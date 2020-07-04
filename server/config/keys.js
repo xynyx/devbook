3d08bfe633923092c327cc5ed123b8d5fac4e06a
xynyx <matt.taylr22@gmail.com>
2020-07-04 19:30:23 -0400

    Set up mongo.db connection

---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // Todo - change dbname
+  mongoURI: "mongodb+srv://xynyx:Pepper20@cluster0.gh1at.mongodb.net/<dbname>?retryWrites=true&w=majority"
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("connected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello"));
 
