44f62ca9d6e1db053e83b431013b2ecd0dae38c1
xynyx <matt.taylr22@gmail.com>
2020-07-09 16:00:29 -0400

    install bootstrap; add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from './components/layout/Navbar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>TEsdST</h1>
+      <Navbar />
     </div>
   );
 }
