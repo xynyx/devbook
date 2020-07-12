78f5a8ed022efa21db6a26b94b62e2f0b5e1cde7
xynyx <matt.taylr22@gmail.com>
2020-07-12 13:32:39 -0400

    add loading spinner

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -19,10 +19,17 @@ var __importStar = (this && this.__importStar) || function (mod) {
     result["default"] = mod;
     return result;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = __importStar(require("react"));
 var react_redux_1 = require("react-redux");
+var react_router_dom_1 = require("react-router-dom");
 var profileActions_1 = require("../../actions/profileActions");
+var Spinner_1 = __importDefault(require("../common/Spinner"));
+// interface ProfileInterface {
+// }
 var Dashboard = /** @class */ (function (_super) {
     __extends(Dashboard, _super);
     function Dashboard() {
@@ -32,8 +39,37 @@ var Dashboard = /** @class */ (function (_super) {
         this.props.getCurrentProfile();
     };
     Dashboard.prototype.render = function () {
-        return react_1.default.createElement("div", null);
+        var user = this.props.auth.user;
+        var _a = this.props.profile, profile = _a.profile, loading = _a.loading;
+        var dashboardContent;
+        if (!profile || loading) {
+            dashboardContent = react_1.default.createElement(Spinner_1.default, null);
+            // If profile 'exists' but has no keys
+        }
+        else if (Object.keys(profile).length > 0) {
+            dashboardContent = react_1.default.createElement("h4", null, "TODO: show profile");
+        }
+        else {
+            // User has no profile
+            dashboardContent = (react_1.default.createElement("div", null,
+                react_1.default.createElement("p", { className: "lead text-muted" },
+                    "Welcome ",
+                    user.name,
+                    " "),
+                react_1.default.createElement("p", null, "Please set up a profile"),
+                react_1.default.createElement(react_router_dom_1.Link, { to: "/create-profile", className: "btn btn-lg btn-info" }, "Create Profile")));
+        }
+        return (react_1.default.createElement("div", { className: "dashboard" },
+            react_1.default.createElement("div", { className: "container" },
+                react_1.default.createElement("div", { className: "row" },
+                    react_1.default.createElement("div", { className: "col-md-12" },
+                        react_1.default.createElement("h1", { className: "display-4" }, "Dashboard"),
+                        dashboardContent)))));
     };
     return Dashboard;
 }(react_1.Component));
-exports.default = react_redux_1.connect(null, { getCurrentProfile: profileActions_1.getCurrentProfile })(Dashboard);
+var mapStateToProps = function (state) { return ({
+    profile: state.profile,
+    auth: state.auth,
+}); };
+exports.default = react_redux_1.connect(mapStateToProps, { getCurrentProfile: profileActions_1.getCurrentProfile })(Dashboard);
--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { getCurrentProfile } from "../../actions/profileActions";
+import { Auth } from "../../types";
+import Spinner from "../common/Spinner";
 
 interface DashboardInterface {
   getCurrentProfile(): any;
+  auth: Auth;
+  //TODO Update later
+  profile: any;
 }
+
+// interface ProfileInterface {
+
+// }
 
 class Dashboard extends Component<DashboardInterface> {
   componentDidMount() {
@@ -12,8 +22,46 @@ class Dashboard extends Component<DashboardInterface> {
   }
 
   render() {
-    return <div></div>;
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+
+    let dashboardContent;
+    if (!profile || loading) {
+      dashboardContent = <Spinner />;
+      // If profile 'exists' but has no keys
+    } else if (Object.keys(profile).length > 0) {
+      dashboardContent = <h4>TODO: show profile</h4>;
+    } else {
+      // User has no profile
+      dashboardContent = (
+        <div>
+          <p className="lead text-muted">Welcome {user.name} </p>
+          <p>Please set up a profile</p>
+          <Link to="/create-profile" className="btn btn-lg btn-info">
+            Create Profile
+          </Link>
+        </div>
+      );
+    }
+
+    return (
+      <div className="dashboard">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4">Dashboard</h1>
+              {dashboardContent}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default connect(null, { getCurrentProfile })(Dashboard);
+const mapStateToProps = (state: any) => ({
+  profile: state.profile,
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
